83b83dd3f1789e1032ea700af19bc27f3212afc5
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-18 19:16:25 +0200

    Added 'guests' and 'admin' folders, and their controllers with routes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guests.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+});
