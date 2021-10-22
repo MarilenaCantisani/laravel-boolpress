c768e6705477ccdb96218d546bdabc7f0f05dacf
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-22 11:57:19 +0200

    Added category to the list of posts in admin.index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -19,6 +19,7 @@
           <tr>
             <th scope="col">#</th>
             <th scope="col">Titolo</th>
+            <th scope="col">Categoria</th>
             <th scope="col">Scritto il</th>
             <th scope="col"></th>
           </tr>
@@ -28,6 +29,7 @@
                 <tr>
                     <th scope="row">{{$post->id}}</th>
                     <td>{{$post->title}}</td>
+                    <td> @if($post->category){{$post->category->name}} @else Nessuna categoria @endif</td>               
                     <td>{{$post->getFormattedDate('created_at')}}</td>
                     <td class="d-flex justify-content-end">
                         <a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">Vedi</a>
